e4cb494cc7725b1bbbde7804ba857518e91dd926
Joseph Knopke <josephknopke@gmail.com>
2021-08-18 13:29:45 -0500

    adding recipe and ingredient classes

---
+++ models/Ingredient.js
@@ -0,0 +1,9 @@
+class Ingredient {
+    
+    constructor(name, recipe_id){
+        this.id = id
+        this.name = name
+        this.recipe_id = recipe_id
+    }
+    
+}
---
+++ models/Recipe.js
@@ -0,0 +1,20 @@
+class Recipe{
+    
+    constructor(title, directions, ingredients){
+        this.id = this.id
+        this.title = title
+        this.directions = directions
+        this.ingredients = [...ingredients]
+    }
+
+    static fetchRecipes(){
+        fetch("http://localhost:3000/recipes")
+        .then(resp => resp.json())
+        .then(json => Recipe.renderRecipes(json))
+    }
+
+    static renderRecipes(recipesInfo){
+        
+    }
+
+}
